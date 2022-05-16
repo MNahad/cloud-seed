d8f001270c745dcd594a433d45b33271ea38be44
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2022-05-16 22:46:13 +0100

    Add allUsers cloudfunction invoker IAM member

--- services/stackbuilder/gcp/gcp.go
+++ services/stackbuilder/gcp/gcp.go
@@ -69,6 +69,9 @@ func NewGcpStack(scope *cdktf.App, id string, config GcpStackConfig) cdktf.Terra
 					schedule.PutHttpTarget(httpTarget)
 				}
 			}
+			if !functionModule.Security.Authentication {
+				newAllUsersCloudFunctionInvoker(&stack, functionModule.Name)
+			}
 		}
 	}
 	return stack
--- services/stackbuilder/gcp/security.go
+++ services/stackbuilder/gcp/security.go
@@ -1,1 +1,22 @@
 package gcp
+
+import (
+	"github.com/aws/jsii-runtime-go"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+	"github.com/mnahad/cloud-seed/generated/google"
+)
+
+func newAllUsersCloudFunctionInvoker(
+	scope *cdktf.TerraformStack,
+	functionName string,
+) *google.CloudfunctionsFunctionIamMember {
+	iamMember := google.NewCloudfunctionsFunctionIamMember(
+		*scope,
+		jsii.String(functionName+"AllUsersInvoker"),
+		&google.CloudfunctionsFunctionIamMemberConfig{
+			CloudFunction: &functionName,
+			Member:        jsii.String("allUsers"),
+			Role:          jsii.String("roles/cloudfunctions.invoker"),
+		})
+	return &iamMember
+}
