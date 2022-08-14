d1d1834a7b45792e01c3d8e14887c8911ebac89f
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2022-08-14 01:59:53 +0100

    Use Cloud Run invoker role instead of Cloud Functions invoker role

--- services/stackbuilder/gcp/security.go
+++ services/stackbuilder/gcp/security.go
@@ -4,19 +4,22 @@ import (
 	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 	"github.com/mnahad/cloud-seed/generated/google"
+	"github.com/mnahad/cloud-seed/generated/google_beta"
+	"github.com/mnahad/cloud-seed/services/config/module"
 )
 
 func newAllUsersCloudFunctionInvoker(
 	scope *cdktf.TerraformStack,
-	functionName string,
-) *google.CloudfunctionsFunctionIamMember {
-	iamMember := google.NewCloudfunctionsFunctionIamMember(
+	function *google_beta.GoogleCloudfunctions2Function,
+	module *module.Module,
+) *google.CloudRunServiceIamMember {
+	iamMember := google.NewCloudRunServiceIamMember(
 		*scope,
-		jsii.String(functionName+"AllUsersInvoker"),
-		&google.CloudfunctionsFunctionIamMemberConfig{
-			CloudFunction: &functionName,
-			Member:        jsii.String("allUsers"),
-			Role:          jsii.String("roles/cloudfunctions.invoker"),
+		jsii.String(module.Name+"AllUsersInvoker"),
+		&google.CloudRunServiceIamMemberConfig{
+			Service: (*function).Name(),
+			Member:  jsii.String("allUsers"),
+			Role:    jsii.String("roles/run.invoker"),
 		})
 	return &iamMember
 }
