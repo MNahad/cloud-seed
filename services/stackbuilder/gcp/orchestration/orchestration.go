65fcc911d0610910bebd786789ee2454c84df135
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2022-09-18 18:46:31 +0100

    Add orchestration service

---
+++ services/stackbuilder/gcp/orchestration/orchestration.go
@@ -0,0 +1,138 @@
+package orchestration
+
+import (
+	"os"
+
+	"github.com/aws/jsii-runtime-go"
+	"github.com/hashicorp/cdktf-provider-google-go/google/v2"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+	"github.com/mnahad/cloud-seed/services/config/module"
+	"github.com/mnahad/cloud-seed/services/config/project"
+)
+
+func NewWorkflow(
+	scope *cdktf.TerraformStack,
+	modules []*module.Module,
+	endpoints *ServiceEndpoints,
+	runtimeServiceAccountEmail *string,
+	options *project.Config,
+) *google.WorkflowsWorkflow {
+	var sourceContents *string
+	if len(options.OrchestrationConfig.Gcp.FilePath) > 0 {
+		contents, err := os.ReadFile(options.OrchestrationConfig.Gcp.FilePath)
+		if err != nil {
+			panic(err)
+		}
+		contentsString := string(contents)
+		sourceContents = &contentsString
+	} else {
+		sourceContents = generateWorkflowSourceContents(modules, endpoints)
+	}
+	workflowConfig := new(google.WorkflowsWorkflowConfig)
+	(*workflowConfig) = options.OrchestrationConfig.Gcp.Config
+	if workflowConfig.Name == nil {
+		workflowConfig.Name = jsii.String(*options.Cloud.Gcp.Provider.Project + "-workflow")
+	}
+	if workflowConfig.SourceContents == nil {
+		workflowConfig.SourceContents = sourceContents
+	}
+	if workflowConfig.Region == nil {
+		workflowConfig.Region = options.Cloud.Gcp.Provider.Region
+	}
+	if workflowConfig.ServiceAccount == nil {
+		workflowConfig.ServiceAccount = runtimeServiceAccountEmail
+	}
+	workflow := google.NewWorkflowsWorkflow(*scope, jsii.String("Workflow"), workflowConfig)
+	return &workflow
+}
+
+func IsWorkflow(o *module.Orchestration) bool {
+	workflow := (module.Orchestration{}.Workflow)
+	return o.Workflow.Start ||
+		o.Workflow.End ||
+		o.Workflow.Input != workflow.Input ||
+		o.Workflow.Output != workflow.Output ||
+		o.Workflow.Next.Jump != workflow.Next.Jump ||
+		len(o.Workflow.Next.Condition) > 0
+}
+
+func generateWorkflowSourceContents(modules []*module.Module, endpoints *ServiceEndpoints) *string {
+	var workflow workflow
+	var steps stepsCollection
+	for i := range modules {
+		moduleConfig := modules[i]
+		workflowConfig := &moduleConfig.Orchestration.Workflow
+		var callStep call
+		callStep.Call = "http.post"
+		callStep.Args.Url = (*endpoints)[moduleConfig.Name].Uri
+		if !moduleConfig.Security.NoAuthentication {
+			callStep.Args.Auth = &callAuth{Type: jsii.String("OIDC")}
+		}
+		if len(workflowConfig.Input.Expression.Gcp) > 0 {
+			callStep.Args.Body = cdktf.Fn_RawString(&workflowConfig.Input.Expression.Gcp)
+		}
+		if len(workflowConfig.Output.Expression.Gcp) > 0 {
+			callStep.Result = cdktf.Fn_RawString(&workflowConfig.Output.Expression.Gcp)
+		}
+		if conditions := workflowConfig.Next.Condition; len(conditions) > 0 {
+			var conditionStep condition
+			conditionStep.Switch = make(conditionSwitch, len(conditions))
+			for j := range conditions {
+				conditionStep.Switch[j].Condition = *cdktf.Fn_RawString(&conditions[j].Expression.Gcp)
+				conditionStep.Switch[j].Next = conditions[j].ServiceName
+			}
+			conditionMap := make(step[condition], 1)
+			conditionMap[moduleConfig.Name+"-switch"] = conditionStep
+			steps.StepsCondition.Steps = append(steps.StepsCondition.Steps, conditionMap)
+			callStep.Next = moduleConfig.Name + "-switch"
+		}
+		if jump := &workflowConfig.Next.Jump; *jump != (module.Orchestration{}.Workflow.Next.Jump) {
+			callStep.Next = jump.ServiceName
+		}
+		if workflowConfig.End {
+			if len(workflowConfig.Output.Expression.Gcp) > 0 {
+				var returnStep ret
+				returnStep.Return = *cdktf.Fn_RawString(
+					jsii.String("${" + workflowConfig.Output.Expression.Gcp + "}"),
+				)
+				returnMap := make(step[ret], 1)
+				returnMap[moduleConfig.Name+"-return"] = returnStep
+				steps.StepsReturn.Steps = append(steps.StepsReturn.Steps, returnMap)
+				callStep.Next = moduleConfig.Name + "-return"
+			} else {
+				callStep.Next = "end"
+			}
+		}
+		callMap := make(step[call], 1)
+		callMap[moduleConfig.Name] = callStep
+		if workflowConfig.Start {
+			steps.StepsCall.Steps = prependStep(steps.StepsCall.Steps, &callMap)
+			if len(workflowConfig.Input.Expression.Gcp) > 0 {
+				workflow.Main.Params = []string{"args"}
+			}
+		} else {
+			steps.StepsCall.Steps = append(steps.StepsCall.Steps, callMap)
+		}
+	}
+	for i := range steps.StepsCall.Steps {
+		workflow.Main.Steps = append(workflow.Main.Steps, steps.StepsCall.Steps[i])
+	}
+	for i := range steps.StepsCondition.Steps {
+		workflow.Main.Steps = append(workflow.Main.Steps, steps.StepsCondition.Steps[i])
+	}
+	for i := range steps.StepsReturn.Steps {
+		workflow.Main.Steps = append(workflow.Main.Steps, steps.StepsReturn.Steps[i])
+	}
+	return cdktf.Fn_Jsonencode(workflow)
+}
+
+func prependStep[T call | condition | ret](slice []step[T], element *step[T]) []step[T] {
+	if len(slice) >= 1 {
+		slice = append(slice, slice[len(slice)-1])
+		copy(slice[1:len(slice)-1], slice[0:len(slice)-2])
+		slice[0] = *element
+	} else {
+		slice = append(slice, *element)
+	}
+	return slice
+}
---
+++ services/stackbuilder/gcp/orchestration/types.go
@@ -0,0 +1,60 @@
+package orchestration
+
+type ServiceEndpoints map[string]Endpoint
+
+type Endpoint struct {
+	Uri string
+}
+
+type step[T call | condition | ret] map[string]T
+
+type steps[T call | condition | ret] struct {
+	Steps []step[T]
+}
+
+type jump struct {
+	Next string `json:"next"`
+}
+
+type call struct {
+	Call string `json:"call"`
+	Args struct {
+		Url  string    `json:"url"`
+		Auth *callAuth `json:"auth"`
+		Body *string   `json:"body"`
+	} `json:"args"`
+	Result *string `json:"result"`
+	jump
+}
+
+type callAuth struct {
+	Type *string `json:"type"`
+}
+
+type condition struct {
+	Switch conditionSwitch `json:"switch"`
+}
+
+type conditionSwitch []struct {
+	Condition string `json:"condition"`
+	jump
+}
+
+type ret struct {
+	Return string `json:"return"`
+}
+
+type stepsCollection struct {
+	StepsCall      steps[call]
+	StepsCondition steps[condition]
+	StepsReturn    steps[ret]
+}
+
+type main struct {
+	Params []string `json:"params"`
+	Steps  []any    `json:"steps"`
+}
+
+type workflow struct {
+	Main main `json:"main"`
+}
